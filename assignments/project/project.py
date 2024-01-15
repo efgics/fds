de5c361a2789a906a1eb7bcd0dc75fa7b8a98976
Erik Golen <efgics@rit.edu>
2024-01-15 11:25:48 -0500

    Testing out moving stuff

---
+++ assignments/project/project.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import time
+
+class my_model():
+    def fit(self, X, y):
+        # do not exceed 29 mins
+        return
+
+    def predict(self, X):
+        # remember to apply the same preprocessing in fit() on test data before making predictions
+        return predictions
+
+
+
