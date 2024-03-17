9a16c3d903f7327f01b8de8d6d0c1e545aa9b3ee
Erik Golen <efgics@rit.edu>
2024-03-17 15:10:09 -0400

    A9

---
+++ assignments/Silhouette/silhouette.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+# Silhouette coefficient must be computed using this equation:
+# si = |1 - (ai/bi)|
+# You will not get the same values if you use a different version of that equation!
+# How you structure your code is up to you
